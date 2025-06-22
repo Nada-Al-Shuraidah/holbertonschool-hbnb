9bbee3cd54f91d44e0239693608499ba42a29a48
Bushraalshulail <109034337+Bushraalshulail@users.noreply.github.com>
2025-06-22 19:57:20 +0300

    Update facade.py

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -8,7 +8,6 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # Placeholder method for creating a user
     def create_user(self, user_data):
         user = User(**user_data)
         self.user_repo.add(user)
@@ -20,7 +19,13 @@ class HBnBFacade:
     def get_user_by_email(self, email):
         return self.user_repo.get_by_attribute('email', email)
 
-    # Placeholder method for fetching a place by ID
     def get_place(self, place_id):
-        # Logic will be implemented in later tasks
         pass
+
+    def update_user(self, user_id, user_data):
+        user = self.user_repo.get(user_id)
+        if not user:
+            return None
+        for key, value in user_data.items():
+            setattr(user, key, value)
+        return user
