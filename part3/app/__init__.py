7aa50b740657edce4a85eec5a871f3ccc7a5521b
Bushraalshulail <109034337+Bushraalshulail@users.noreply.github.com>
2025-07-01 20:26:43 +0300

    some modification __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -6,23 +6,23 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
-from app.api.v1.auth import api as auth_ns  # تأكد من وجود هذا الملف والـ namespace
+from app.api.v1.auth import api as auth_ns 
 
 def create_app(config_class='config.DevelopmentConfig'):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # تهيئة JWTManager
+
     jwt = JWTManager(app)
 
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
-    # تسجيل الـ namespaces
+  
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
-    api.add_namespace(auth_ns, path='/api/v1/auth')  # إضافة namespace الخاصة بالتوثيق
+    api.add_namespace(auth_ns, path='/api/v1/auth')
 
     print(app.url_map)
     return app
