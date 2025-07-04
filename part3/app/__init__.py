3ac22f0610ecced8fc95b981a89a094fd44b6d94
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-07-05 01:34:54 +0300

    Update __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -3,6 +3,7 @@ from flask_restx import Api
 from config import config
 from app.extensions import db, jwt, bcrypt
 
+# Import namespaces (API Blueprints)
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
@@ -13,10 +14,12 @@ def create_app(config_class=config['default']):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    # Initialize extensions
     db.init_app(app)
     jwt.init_app(app)
     bcrypt.init_app(app)
 
+    # Register API namespaces
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
     api.add_namespace(users_ns, path='/api/v1/users')
