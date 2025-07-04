7415a86bb47b7f47ef85da0bcd5309477f2ed850
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-07-05 01:02:30 +0300

    Update __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,25 +1,21 @@
 from flask import Flask
 from flask_restx import Api
-from flask_jwt_extended import JWTManager
-from flask_bcrypt import Bcrypt 
-from config import DevelopmentConfig
-from app import db
+from config import config
+from app.extensions import db, jwt, bcrypt
+
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
-from app.api.v1.auth import api as auth_ns 
+from app.api.v1.auth import api as auth_ns
 
-bcrypt = Bcrypt() 
-jwt = JWTManager()
-
-def create_app(config_class='config.DevelopmentConfig'):
+def create_app(config_class=config['default']):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    db.init_app(app)
     jwt.init_app(app)
     bcrypt.init_app(app)
-    db.init_app(app)
 
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
@@ -29,5 +25,4 @@ def create_app(config_class='config.DevelopmentConfig'):
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(auth_ns, path='/api/v1/auth')
 
-    print(app.url_map)
     return app
