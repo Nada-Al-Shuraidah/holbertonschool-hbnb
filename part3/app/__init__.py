8c9de8214a3bd7c583637476f322146b8a93d808
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-07-04 23:15:21 +0300

    Update __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,33 +1,33 @@
 from flask import Flask
 from flask_restx import Api
 from flask_jwt_extended import JWTManager
-from flask_bcrypt import Bcrypt  # ندى
-from config import DevelopmentConfig  # استيراد كلاس الكونفيج الافتراضي . تعديل ندى
-
+from flask_bcrypt import Bcrypt 
+from config import DevelopmentConfig
+from app import db
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.auth import api as auth_ns 
 
-bcrypt = Bcrypt()  # ندى
+bcrypt = Bcrypt() 
 jwt = JWTManager()
+
 def create_app(config_class='config.DevelopmentConfig'):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-
-    jwt = JWTManager(app)
+    jwt.init_app(app)
+    bcrypt.init_app(app)
+    db.init_app(app)
 
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
-  
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(auth_ns, path='/api/v1/auth')
-    
-    bcrypt.init_app(app)  # ندى
+
     print(app.url_map)
     return app
