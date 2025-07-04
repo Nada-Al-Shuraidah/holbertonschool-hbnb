1c8a50b46d748c66f85c6285aafb43c1bde95c79
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-07-05 02:00:46 +0300

    Update __init__.py

--- part3/app/api/v1/__init__.py
+++ part3/app/api/v1/__init__.py
@@ -1,2 +1,9 @@
+from flask_restx import Api
+
+api = Api()
+
 from .auth import api as auth_ns
+from .users import api as users_ns
+
 api.add_namespace(auth_ns, path='/auth')
+api.add_namespace(users_ns, path='/users')
