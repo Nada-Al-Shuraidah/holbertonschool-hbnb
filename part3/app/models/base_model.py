73bb4a7620f413b13129989462508e74308133fa
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-07-05 01:39:47 +0300

    Update base_model.py

--- part3/app/models/base_model.py
+++ part3/app/models/base_model.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from app.extensions import db
 
 class BaseModel(db.Model):
-    __abstract__ = True  # يمنع SQLAlchemy من إنشاء جدول لهذا الكلاس
+    __abstract__ = True
 
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
