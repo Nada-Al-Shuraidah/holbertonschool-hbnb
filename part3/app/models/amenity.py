bcd3a9f440f39f49c93d0e2050d56785d5505fef
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-07-05 22:01:08 +0300

    Update amenity.py

--- part3/app/models/amenity.py
+++ part3/app/models/amenity.py
@@ -1,9 +1,16 @@
 from .base_model import BaseModel
 from app.extensions import db
+from .place_amenity import place_amenity
 
 class Amenity(BaseModel):
     __tablename__ = 'amenities'
 
     name = db.Column(db.String(50), nullable=False)
 
-    # places relationship is declared via backref in Place
+    # Many-to-Many: Places that include this amenity
+    places = db.relationship(
+        "Place",
+        secondary=place_amenity,
+        backref=db.backref("amenities", lazy="subquery"),
+        lazy="subquery"
+    )
