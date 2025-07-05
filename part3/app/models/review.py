de31db867bbff1c2db8af1c54652f37d3792134a
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-07-05 20:12:59 +0300

    Update review.py

--- part3/app/models/review.py
+++ part3/app/models/review.py
@@ -1,26 +1,8 @@
 from .base_model import BaseModel
-from .place      import Place
-from .user       import User
+from app.extensions import db
 
 class Review(BaseModel):
-    """A user’s feedback on a specific place."""
+    __tablename__ = 'reviews'
 
-    def __init__(self, text, rating, place, user):
-        super().__init__()
-
-        # Validate related objects
-        if not isinstance(place, Place):
-            raise TypeError("Place must be a Place instance.")
-        if not isinstance(user, User):
-            raise TypeError("User must be a User instance.")
-
-        # Validate rating
-        val = int(rating)
-        if not 1 <= val <= 5:
-            raise ValueError("Rating must be an integer between 1 and 5.")
-
-        # Assign attributes
-        self.text   = text
-        self.rating = val
-        self.place  = place
-        self.user   = user
+    text = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
