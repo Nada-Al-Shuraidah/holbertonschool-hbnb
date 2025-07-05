8b238f30aa7aabb08f96f06df69b8323baa79205
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-07-05 21:52:49 +0300

    Update review.py

--- part3/app/models/review.py
+++ part3/app/models/review.py
@@ -6,3 +6,7 @@ class Review(BaseModel):
 
     text = db.Column(db.Text, nullable=False)
     rating = db.Column(db.Integer, nullable=False)
+
+    # Foreign keys
+    place_id = db.Column(db.String(36), db.ForeignKey('places.id'), nullable=False)
+    user_id  = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
